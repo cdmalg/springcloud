db02ab5232a9f2f178c0513c29833cf3ad89d630
cdmalg <cdmalg@outlook.com>
2019-06-27 18:28:32 +0800

    基于ribbon的hystrix熔断

---
+++ ribbon-hystrix/src/main/java/com/cloud/ribbon/hystrix/RibbonHystrixApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.ribbon.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonHystrixApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RibbonHystrixApplication.class, args);
+  }
+
+}
